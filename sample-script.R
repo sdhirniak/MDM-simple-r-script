a674f7b7d751bd26146afe9f784684984baa1d19
Jacob Jameson <jacobjameson@g.harvard.edu>
2023-10-18 14:04:48 -0400

    Add files via upload

---
+++ sample-script.R
@@ -0,0 +1,14 @@
+#--------------------------------------------------------------
+# Author: [Replace with your name]
+#
+# Purpose: Make simple, local edits to an R script and push 
+#          them to GitHub
+#
+# Last Updated: [Replace with today's date]
+#--------------------------------------------------------------
+
+# read in the sample data -------------------------------------
+data <- read.csv('data.csv')
+
+
+# calculate summary statistics --------------------------------
